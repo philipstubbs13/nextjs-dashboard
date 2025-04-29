7107a2ceab63f69cf835bf615114a7868f625295
Phil Stubbs <philipstubbs13@gmail.com>
2025-04-29 04:08:06 -0500

    fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: {
@@ -18,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <Suspense>{children}</Suspense>
+      </body>
     </html>
   );
 }
